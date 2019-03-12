9017d6b45da86cd71448e6d06196bb43400547b4
Grzegorz Nowiński (GRNO) <grno@dgs.com>
2019-03-12 22:15:02 +0100

    Added another task

--- SimpleCalculator.py
+++ SimpleCalculator.py
@@ -1,13 +1,5 @@
-import unittest
-
-# Input:
-#    - operation - string value specifying operation, e.g.: add, subtract, divide, multiply
-#    - first_num - value of first number
-#    - second_num - value of second number
-# Output:
-#    - function should return result of operation
-
 def calculate(operation, first_num, second_num):
+    result = 0
     if operation == "add":
         result = first_num + second_num
 
@@ -17,22 +9,28 @@ def calculate(operation, first_num, second_num):
     elif operation == "multiply":
         result = first_num * second_num
 
-
     elif operation == "divide":
-        result = first_num  / second_num
-        if second_num == 0:
-            result = 0
-
-
-
-
-
-
+        if second_num != 0:
+            result = first_num / second_num
 
     return result
 
 
-calculate("add", 100, 0)
+# you need to implement this function,
+# INPUT:    as parameter it should take list of list, see example_list below,
+# OUTPUT:   it should return list of calculated values
+#    This function should use calculate function on parameters stored in list os lists passed as parameter
+def calculate_many_records(list_of_records):
+    result_list = []
+    # somewhere here you should create your implementation
+    return result_list
 
 
-print(calculate("add", 100, 0))
+# lines below are another way to look if your code, it is example how this code could be used
+# you can also use it to check how it works
+example_list = [["add", 0, 100],
+                ["subtract", 10, 50],
+                ["multiply", 10, 50],
+                ["divide", 10, 50]]
+
+print(calculate_many_records(example_list))
--- SimpleCalculatorTests.py
+++ SimpleCalculatorTests.py
@@ -5,85 +5,105 @@ from SimpleCalculator import *
 class TestAddOperation(unittest.TestCase):
 
     def test_add_two_zeros(self):
-        self.assertEqual(calculate("add", 0, 0), 0)
+        self.assertEqual(0, calculate("add", 0, 0))
+
+    def test_add_zero_to_value(self):
+        self.assertEqual(100, calculate("add", 100, 0))
 
     def test_add_two_ints(self):
-        self.assertEqual(calculate("add", 100, 1), 101)
+        self.assertEqual(101, calculate("add", 100, 1))
 
     def test_add_minus_int(self):
-        self.assertEqual(calculate("add", 100, -101), -1)
+        self.assertEqual(-1, calculate("add", 100, -101))
 
     def test_add_two_floats(self):
-        self.assertEqual(calculate("add", 3.5, 3.6), 7.1)
+        self.assertEqual(7.1, calculate("add", 3.5, 3.6))
 
     def test_add_float_to_int(self):
-        self.assertEqual(calculate("add", 7, 3.5), 10.5)
+        self.assertEqual(10.5, calculate("add", 7, 3.5))
 
 
 class TestSubtractOperation(unittest.TestCase):
 
     def test_subtract_two_zeros(self):
-        self.assertEqual(calculate("subtract", 0, 0), 0)
+        self.assertEqual(0, calculate("subtract", 0, 0))
 
     def test_subtract_smaller_value(self):
-        self.assertEqual(calculate("subtract", 100, 1), 99)
+        self.assertEqual(99, calculate("subtract", 100, 1))
 
     def test_subtract_bigger_value(self):
-        self.assertEqual(calculate("subtract", 1, 100), -99)
+        self.assertEqual(-99, calculate("subtract", 1, 100))
 
     def test_subtract_minus_value(self):
-        self.assertEqual(calculate("subtract", 100, -101), 201)
+        self.assertEqual(201, calculate("subtract", 100, -101))
 
     def test_subtract_from_minus_value(self):
-        self.assertEqual(calculate("subtract", -10, 50), -60)
+        self.assertEqual(-60, calculate("subtract", -10, 50))
 
     def test_subtract_float_from_int(self):
-        self.assertEqual(calculate("subtract", 7, 3.5), 3.5)
+        self.assertEqual(3.5, calculate("subtract", 7, 3.5))
 
 
 class TestMultiplyOperation(unittest.TestCase):
 
     def test_multiply_two_zeros(self):
-        self.assertEqual(calculate("multiply", 0, 0), 0)
+        self.assertEqual(0, calculate("multiply", 0, 0))
 
     def test_multiply_plus_values(self):
-        self.assertEqual(calculate("multiply", 100, 1), 100)
+        self.assertEqual(100, calculate("multiply", 100, 1))
 
     def test_multiply_one_minus_value(self):
-        self.assertEqual(calculate("multiply", 1, -100), -100)
+        self.assertEqual(-100, calculate("multiply", 1, -100))
 
     def test_multiply_two_minus_value(self):
-        self.assertEqual(calculate("multiply", -100, -2), 200)
+        self.assertEqual(200, calculate("multiply", -100, -2))
 
     def test_multiply_float_with_int(self):
-        self.assertAlmostEqual(calculate("multiply", 7, 3.5), 24.5)
+        self.assertAlmostEqual(24.5, calculate("multiply", 7, 3.5))
 
     def test_multiply_two_floats(self):
-        self.assertAlmostEqual(calculate("multiply", 4.2, 3.5), 14.7)
+        self.assertAlmostEqual(14.7, calculate("multiply", 4.2, 3.5))
 
 
 class TestDivideOperation(unittest.TestCase):
 
     def test_divide_by_zeros(self):
-        self.assertEqual(calculate("divide", 100, 0), 0)
+        self.assertEqual(0, calculate("divide", 100, 0))
 
     def test_divide_two_zeros(self):
-        self.assertEqual(calculate("divide", 0, 0), 0)
+        self.assertEqual(0, calculate("divide", 0, 0))
 
     def test_divide_plus_values(self):
-        self.assertEqual(calculate("divide", 100, 2), 50)
+        self.assertEqual(50, calculate("divide", 100, 2))
 
     def test_divide_one_minus_value(self):
-        self.assertEqual(calculate("divide", 100, -50), -2)
+        self.assertEqual(-2, calculate("divide", 100, -50))
 
     def test_divide_two_minus_value(self):
-        self.assertEqual(calculate("divide", -100, -2), 50)
+        self.assertEqual(50, calculate("divide", -100, -2))
 
     def test_divide_float_with_int(self):
-        self.assertAlmostEqual(calculate("divide", 7, 3.5), 2)
+        self.assertAlmostEqual(2, calculate("divide", 7, 3.5))
 
     def test_divide_two_floats(self):
-        self.assertAlmostEqual(calculate("divide", 4.2, 3.5), 1.2)
+        self.assertAlmostEqual(1.2, calculate("divide", 4.2, 3.5))
+
+
+class TestCalculateManyRecords(unittest.TestCase):
+
+    def test_calculate_many_records(self):
+        input_list = [["add", 0, 101],
+                      ["add", 2, 101],
+                      ["subtract", 10, 50],
+                      ["subtract", 20, 10],
+                      ["multiply", 10, 5],
+                      ["multiply", 3, 0],
+                      ["divide", 10, 50],
+                      ["divide", 10, 0]]
+
+        output_list = [101, 103, -40, 10, 50, 0, 0.2, 0]
+
+        self.assertEqual(output_list, calculate_many_records(input_list))
 
 
 if __name__ == '__main__':
